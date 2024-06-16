d38fc8e6edec63ce6f16186a98ae75c1e273ff84
Torstein Grindvik <torstein.grindvik@muybridge.com>
2024-06-16 10:23:09 +0200

    Fft stuff
    Signed-off-by: Torstein Grindvik <torstein.grindvik@muybridge.com>

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -2,10 +2,145 @@
 
 use bevy::prelude::*;
 
+// fn main() {
+//     let file_appender = tracing_appender::rolling::hourly(".", "my-funny-foo.log");
+//     let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+//     bevy::log::tracing_subscriber::fmt()
+//         .with_writer(non_blocking)
+//         .init();
+
+//     App::new()
+//         .add_plugins(DefaultPlugins)
+//         .add_systems(Startup, |mut commands: Commands| {
+//             commands.spawn(Camera3dBundle {
+//                 transform: Transform::from_xyz(0.0, 2.5, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
+//                 ..default()
+//             });
+//         })
+//         .add_systems(Update, |mut gizmos: Gizmos| {
+//             gizmos.arrow(Vec3::ZERO, Vec3::ONE, Color::default());
+//         })
+//         .run();
+// }
+
+#[path = "helpers/camera_controller.rs"]
+mod camera_controller;
+
+use bevy::{color::palettes, prelude::*};
+use camera_controller::{CameraController, CameraControllerPlugin};
+
 fn main() {
-    App::new().add_systems(Update, hello_world_system).run();
+    let file_appender = tracing_appender::rolling::hourly(".", "my-funny-foo.log");
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+    bevy::log::tracing_subscriber::fmt()
+        .with_writer(non_blocking)
+        .init();
+
+    App::new()
+        .add_plugins((DefaultPlugins, CameraControllerPlugin))
+        .add_systems(Startup, setup)
+        .add_systems(Update, (update, move_balls, despawn_balls).chain())
+        .observe(spinny_boy)
+        .run();
 }
 
-fn hello_world_system() {
-    println!("hello world");
+#[derive(Debug, Event)]
+struct FourierEval {
+    pos: Vec3,
+    color: Color,
+}
+
+#[derive(Debug, Component)]
+struct Ballz;
+
+fn spinny_boy(
+    trigger: Trigger<FourierEval>,
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut mesh: Local<Option<Handle<Mesh>>>,
+) {
+    let FourierEval { pos, color } = trigger.event();
+
+    let mesh_handle = mesh
+        .get_or_insert(meshes.add(Sphere::default().mesh().ico(5).unwrap()))
+        .clone_weak();
+    let material = materials.add(StandardMaterial::from(*color));
+
+    commands.spawn((
+        PbrBundle {
+            mesh: mesh_handle,
+            material: Default::default(),
+            transform: Transform::from_translation(*pos).with_scale(Vec3::splat(0.025)),
+            ..default()
+        },
+        Ballz,
+    ));
+}
+
+fn move_balls(t: Res<Time>, mut q: Query<&mut Transform, With<Ballz>>) {
+    let dt = t.delta_seconds();
+
+    q.par_iter_mut().for_each(|mut transform| {
+        transform.translation += dt * Vec3::NEG_Z * 1.5;
+    });
+}
+
+fn despawn_balls(mut commands: Commands, q: Query<(Entity, &Transform), With<Ballz>>) {
+    for (entity, transform) in &q {
+        if transform.translation.z < -10.0 {
+            commands.entity(entity).despawn();
+        }
+    }
+}
+
+fn setup(mut commands: Commands) {
+    // camera
+    commands.spawn((
+        Camera3dBundle {
+            transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+            ..default()
+        },
+        CameraController::default(),
+    ));
+}
+
+// fn signal(time: f32) -> f32 {}
+
+fn update(mut commands: Commands, mut gizmos: Gizmos, t: Res<Time>) {
+    let colors = [
+        palettes::tailwind::CYAN_100,
+        palettes::tailwind::EMERALD_300,
+        palettes::tailwind::FUCHSIA_400,
+        palettes::tailwind::GREEN_300,
+        palettes::tailwind::NEUTRAL_500,
+        palettes::tailwind::ORANGE_400,
+        palettes::tailwind::ROSE_700,
+        palettes::tailwind::ROSE_700,
+        palettes::tailwind::SKY_800,
+        palettes::tailwind::YELLOW_500,
+    ];
+
+    // I need to sum over time, not use elapsed time.
+    // And likely lots of timesteps
+    for index in 0..=9 {
+        // From 0.1 to 1.0 Hz
+        let freq = (index as f32 + 1.0) / 10.0 * std::f32::consts::TAU;
+        let elapsed = t.elapsed_seconds();
+        let center = Vec3::new(index as f32, 0.0, 0.0);
+
+        let (im, re) = (elapsed * freq).sin_cos();
+        let amplitude = 1. / 3.;
+        let signal = elapsed.sin();
+
+        let pos = center + Vec3::new(re, im, 0.0) * amplitude * signal;
+
+        let color = colors[index];
+        gizmos.arrow(center, pos, color);
+
+        commands.trigger(FourierEval {
+            pos,
+            color: color.into(),
+        });
+    }
 }
--- examples/shader/fft.rs
+++ examples/shader/fft.rs
@@ -1,10 +1,17 @@
 //! Hey
 
-use bevy::{color::palettes, prelude::*};
+#[path = "../helpers/camera_controller.rs"]
+mod camera_controller;
+
+use std::f32::consts::PI;
+
+use bevy::{color::palettes, math::NormedVectorSpace, prelude::*, utils::HashMap};
+use camera_controller::{CameraController, CameraControllerPlugin};
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .init_resource::<Colors>()
+        .add_plugins((DefaultPlugins, CameraControllerPlugin))
         .add_systems(Startup, setup)
         .add_systems(Update, (update, move_balls, despawn_balls).chain())
         .observe(spinny_boy)
@@ -13,29 +20,68 @@ fn main() {
 
 #[derive(Debug, Event)]
 struct FourierEval {
+    index: usize,
     pos: Vec3,
 }
 
 #[derive(Debug, Component)]
 struct Ballz;
 
+#[derive(Debug, Resource, Deref)]
+struct Colors(HashMap<usize, Color>);
+
+impl Default for Colors {
+    fn default() -> Self {
+        Self(HashMap::from_iter(
+            [
+                palettes::tailwind::CYAN_100,
+                palettes::tailwind::EMERALD_300,
+                palettes::tailwind::FUCHSIA_400,
+                palettes::tailwind::GREEN_300,
+                palettes::tailwind::NEUTRAL_500,
+                palettes::tailwind::ORANGE_400,
+                palettes::tailwind::ROSE_700,
+                palettes::tailwind::ROSE_700,
+                palettes::tailwind::SKY_800,
+                palettes::tailwind::YELLOW_500,
+            ]
+            .into_iter()
+            .map(Into::into)
+            .enumerate(),
+        ))
+    }
+}
+
 fn spinny_boy(
     trigger: Trigger<FourierEval>,
+    colors: Res<Colors>,
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
     mut mesh: Local<Option<Handle<Mesh>>>,
+    mut mat: Local<HashMap<usize, Handle<StandardMaterial>>>,
 ) {
-    let FourierEval { pos } = trigger.event();
+    let FourierEval { index, pos } = trigger.event();
 
     let mesh_handle = mesh
         .get_or_insert(meshes.add(Sphere::default().mesh().ico(5).unwrap()))
+        .clone_weak();
+    let mat_handle = mat
+        .entry(*index)
+        .or_insert_with(|| {
+            materials.add({
+                let mut mat = StandardMaterial::from(colors[index]);
+                mat.unlit = true;
+                mat
+            })
+        })
         .clone_weak();
 
     commands.spawn((
         PbrBundle {
             mesh: mesh_handle,
-            material: Default::default(),
-            transform: Transform::from_translation(*pos).with_scale(Vec3::splat(0.005)),
+            material: mat_handle,
+            transform: Transform::from_translation(*pos).with_scale(Vec3::splat(0.025)),
             ..default()
         },
         Ballz,
@@ -46,14 +92,13 @@ fn move_balls(t: Res<Time>, mut q: Query<&mut Transform, With<Ballz>>) {
     let dt = t.delta_seconds();
 
     q.par_iter_mut().for_each(|mut transform| {
-        transform.translation += dt * Vec3::NEG_Z * 0.1;
-        // transform.scale *= Vec3::splat(0.99);
+        transform.translation += dt * Vec3::NEG_Z * 1.5;
     });
 }
 
 fn despawn_balls(mut commands: Commands, q: Query<(Entity, &Transform), With<Ballz>>) {
     for (entity, transform) in &q {
-        if transform.translation.z < -1.0 {
+        if transform.translation.z < -10.0 {
             commands.entity(entity).despawn();
         }
     }
@@ -61,19 +106,88 @@ fn despawn_balls(mut commands: Commands, q: Query<(Entity, &Transform), With<Bal
 
 fn setup(mut commands: Commands) {
     // camera
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ..default()
-    });
+    commands.spawn((
+        Camera3dBundle {
+            transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+            ..default()
+        },
+        CameraController::default(),
+    ));
 }
 
-fn update(mut commands: Commands, mut gizmos: Gizmos, t: Res<Time>) {
-    let v0 = Vec3::ZERO;
+fn x(n: f32) -> f32 {
+    let k = 5.0;
+    (2. * PI * k * n).sin()
+}
 
-    let (im, re) = t.elapsed_seconds().sin_cos();
-    let pos = Vec3::new(re, im, 0.0);
+fn update(
+    // mut commands: Commands,
+    // mut gizmos: Gizmos,
+    // t: Res<Time>,
+    // colors: Res<Colors>,
+    mut iter: Local<usize>,
+) {
+    let mut max_sum = -1.0;
+    let mut max_k = usize::MAX;
 
-    gizmos.arrow(v0, pos, palettes::tailwind::AMBER_100);
+    const N: usize = 10000;
 
-    commands.trigger(FourierEval { pos });
+    if *iter == 0 {
+        info!("gooing for it");
+    }
+
+    for k in 1..=(N / 2) {
+        let mut sum = 0.0;
+
+        for n in 0..N {
+            let n = n as f32;
+            let k = k as f32;
+
+            sum += x(n) * (2. * PI * k * n / N as f32).cos();
+
+            // let freq = index as f32 / 10.0;
+            // // let start = Vec3::new(index as f32, 0.0, 0.0);
+
+            // let mut sum = 0.0;
+            // for t in 0..100_000 {
+            //     let t = t as f32 / 1000.0;
+
+            //     let re = (freq * t * std::f32::consts::TAU).cos();
+            //     // sum += g(t) * Vec2::new(re, im).length();
+            //     sum += g(t) * re;
+            // }
+
+            // // sum /= 1000.0;
+
+            // // let pos = center + Vec3::new(re, im, 0.0) * amplitude * signal;
+
+            // // let color = colors[&index];
+            // let ampl = Vec3::Y * sum;
+            // // gizmos.arrow(start, start + ampl, color);
+
+            // // commands.trigger(FourierEval { pos: ampl, index });
+
+            // if *iter == 0 {
+            //     info!("f:{freq:.2}, index {index}, sum:{sum:.2}");
+            // }
+            // if sum > max_sum {
+            //     max_sum = sum;
+            //     max_index = index;
+            // }
+        }
+
+        // sum *= 2.0;
+        // sum /= N as f32;
+
+        if sum > max_sum {
+            max_sum = sum;
+            max_k = k;
+        }
+    }
+
+    if *iter == 0 {
+        info!("max sum: {max_sum:.8} @ {max_k}");
+    }
+
+    *iter += 1;
 }
